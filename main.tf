309dc9221545463aba9c738616f654e40b8ddce2
ian blanshard <ian246@hotmail.co.uk>
2026-06-17 11:15:04 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "eu-west-2"
+  profile = "ianb-terraform-user"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "app_server_ianb" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-05985aecc5497ed1a"]
+
+
+  tags = {
+    Name = "ianb_learn-terraform"
+  }
+}
+
+resource "aws_s3_bucket" "ianb-terraform-bucket" {
+	bucket = "ianb-terraform-bucket"
+	tags = {
+    Name = "ianb-learn-terraform"
+  }
+}
+
---
+++ terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+
+backend "s3" {
+    bucket = "ianb-terraform-state-bucket"
+    key    = "ianb/state"
+    region = "eu-west-2"
+    profile = "ianb-terraform-user"
+  }
+}
+
